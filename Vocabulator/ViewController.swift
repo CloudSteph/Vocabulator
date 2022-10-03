e1e25e65eaba7037f8cc3b903960826b1306401a
Stephanie Liew <liewstephanie88@gmail.com>
2022-10-03 09:44:10 -0400

    Initial Commit

---
+++ Vocabulator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Vocabulator
+//
+//  Created by Stephanie Liew on 10/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
